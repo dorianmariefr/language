31aa90f61e878d8ed29b0963e8095dfe4fdf6499
Dorian Marié <dorian@thoughtbot.com>
2022-07-14 13:19:10 +0200

    is_*? methods

--- src/language/output.cr
+++ src/language/output.cr
@@ -17,12 +17,32 @@ class Language
       @raw != nil
     end
 
+    def is_nil?
+      @raw.is_a?(Nil)
+    end
+
+    def as_nil
+      @raw.as(Nil)
+    end
+
+    def is_string?
+      @raw.is_a?(String)
+    end
+
     def as_s : String
       @raw.as(String)
     end
 
+    def is_array?
+      @raw.is_a?(Array(Output))
+    end
+
     def as_a : Array(Output)
       @raw.as(Array(Output))
+    end
+
+    def is_hash?
+      @raw.is_a?(Hash(Symbol, Output))
     end
 
     def as_h : Hash(Symbol, Output)
